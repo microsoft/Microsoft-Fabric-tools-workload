d3a21bf847df8d6e6a5e296b2c56f5a04fd5e144
Gerd Saurer <gerd.saurer@microsoft.com>
2025-12-18 14:45:35 +0100

    adding index.tsx

--- Workload/app/items/ExcelEditorItem/index.tsx
+++ Workload/app/items/ExcelEditorItem/index.tsx
@@ -1,1 +1,7 @@
 export { ExcelEditorItemEditor } from './ExcelEditorItemEditor';
+export { ExcelEditorItemDefaultView } from './ExcelEditorItemDefaultView';
+export { ExcelEditorItemEmptyView } from './ExcelEditorItemEmptyView';
+export { ExcelEditorItemRibbon } from './ExcelEditorItemRibbon';
+export { ExcelEditorItemDetailView } from './ExcelEditorItemDetailView';
+
+export type { ExcelEditorItemDefinition, CanvasItem } from './ExcelEditorItemDefinition';
